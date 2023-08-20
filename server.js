567c700fb5578ef760b45b7701e6d25a95d2bfe9
OlhaYastrebova <116676978+OlhaYastrebova@users.noreply.github.com>
2023-08-20 18:18:09 +0300

    add .env

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST="mongodb+srv://OlhaYastrebova:N5SugRgyKZ9SLr3T@cluster0.jio5oz9.mongodb.net/db_contacts?retryWrites=true&w=majority"
-// const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT } = process.env;
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000)
-    // app.listen(PORT, () => {
-    //   console.log(`Server running. Use our API on port: ${PORT}`);
-    // });
+      app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
     console.log("Database connect success");
   })
   .catch((error) => {
