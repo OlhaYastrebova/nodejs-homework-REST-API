94f1ed19c41e1bd2d91f613fd57a1e7d5f1e71b1
OlhaYastrebova <116676978+OlhaYastrebova@users.noreply.github.com>
2023-08-20 22:49:08 +0300

    hw-3

---
+++ models/contact.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+const Contact = model("contact", contactSchema);
+
+const addSchema = Joi.object({
+  name: Joi.string().min(3).required()
+  .messages({"any.required": `missing required name field`}),
+  email: Joi.string().required()
+  .messages({"any.required": `missing required email field`}),
+    phone: Joi.string().min(6).required()
+    .messages({"any.required": `missing required phone field`}),
+  favorite: Joi.boolean().required()
+  .messages({"any.required": `missing required favorite field`}),
+});
+
+// const updFavoriteSchema = Joi.object({
+//   favorite: Joi.boolean().required(),
+// });
+
+const schemas = {
+  addSchema,
+  // updFavoriteSchema,
+};
+
+module.exports = { Contact, schemas };
--- server.js
+++ server.js
@@ -18,4 +18,5 @@ mongoose
     process.exit(1);
   });
 
+  
  
